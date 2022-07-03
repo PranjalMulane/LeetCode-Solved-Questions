c9a6fd378b60fa85e57f70f57777b95244ab7e0f
Pranjal Mulane <52007480+PranjalMulane@users.noreply.github.com>
2022-07-03 22:31:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.02%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        if( x == 1) return 1;
+        if( x == 0) return 0;
+        
+        int l=1, h=x/2;
+        int ans;
+        
+
+        
+        while( l<=h ){
+            long long int mid = l+ ((h-l)/2);
+            long long int sqr = mid*mid;
+            
+            if( sqr > x ){
+                h = mid -1;
+            }
+            else if ( sqr < x ){
+                ans = mid;
+                l = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return ans;
+    }
+};
