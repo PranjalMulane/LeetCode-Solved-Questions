f1456bdfcef959881e8ee685535c7bb1f85d7060
Pranjal Mulane <52007480+PranjalMulane@users.noreply.github.com>
2022-07-04 07:15:17 +0530

    Added solution - LeetHub

--- Smaller and Larger - GFG/smaller-and-larger.cpp
+++ Smaller and Larger - GFG/smaller-and-larger.cpp
@@ -11,21 +11,47 @@ public:
 	vector<int> getMoreAndLess(int arr[], int n, int x) {
 	    // code here
 	    vector<int> v;
-	    int count1=0;
-	    int count2=0;
-	    for(int i=0;i<n;i++){
-	        if( arr[i] <= x){
+	    int l=0,h = n;
+	    int count1 =0;
+	    int count2 =0;
+	    while(l<h){
+	        if(arr[l] <= x){
 	            count1++;
-	            
+	           
 	        }
-	        if( arr[i] >= x){
+	        if(arr[l] >= x ){
 	            count2++;
-	            
+	           
 	        }
+	        l++;
+	        
+	        
 	    }
 	    v.push_back(count1);
 	    v.push_back(count2);
 	    return v;
+	    
+	    
+	    
+	   // int count1=0;
+	   // int count2=0;
+	   // for(int i=0;i<n;i++){
+	   //     if( arr[i] <= x){
+	   //         count1++;
+	            
+	   //     }
+	   //     if( arr[i] >= x){
+	   //         count2++;
+	            
+	   //     }
+	   // }
+	   // v.push_back(count1);
+	   // v.push_back(count2);
+	   // return v;
+	    
+	    
+	    
+	    
 	}
 };
 
