b563a66d5338e170a98496c0a0ac20ca6a43964f
Pranjal Mulane <52007480+PranjalMulane@users.noreply.github.com>
2022-07-05 08:21:50 +0530

    Added solution - LeetHub

---
+++ Peak element - GFG/peak-element.c
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int peakElement(int arr[], int n)
+{
+   // Your code here
+   int l=0;
+   int h=n-1;
+   int mid;
+   while(l<=h){
+       mid = l + ((h-l)/2);
+       
+       if((mid == 0 ||arr[mid-1] <= arr[mid]) && (mid == n-1||arr[mid+1] <=arr[mid])){
+           return mid;
+       }
+       
+       if((mid>0) && arr[mid-1] > arr[mid]){
+           h = mid-1;
+       }
+       else{
+           l = mid+1;
+       }
+       
+   }
+   return mid;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		int n;
+		scanf("%d", &n);
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			scanf("%d", &a[i]);
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		
+		int A = peakElement(tmp,n);
+		
+		if(A<0 && A>=n)
+		    printf("0\n");
+		else
+		{
+    		if(n==1 && A==0)
+    		    f=1;
+    		else if(A==0 && a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 && a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] && a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		printf("%d\n", f);
+		}
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
